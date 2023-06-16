4ec3eea0c847148bbe6348136e93b4a8aa5cc600
Adnane El ghazi <lhouma2000@gmail.com>
2023-06-16 22:03:50 +0200

    End of section 6

--- MariageApp-SPA/src/app/_services/auth.service.ts
+++ MariageApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  jwtHelper =new JwtHelperService ();
   baseUrl = 'http://localhost:5112/auth/';
+  decodedToken :any;
 
 constructor(private http: HttpClient) { }
 login(model: any) {
@@ -14,6 +17,8 @@ login(model: any) {
     map((response: any) => {
       const user = response;
       if (user) { localStorage.setItem('token', user.token);
+      this.decodedToken=this.jwtHelper.decodeToken(user.token);
+      console.log(this.decodedToken);
     
     }
     }))
@@ -21,4 +26,22 @@ login(model: any) {
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
 }
+loggedIn() {
+    
+  try{const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);}
+  catch{
+    return false
+  }
+    
+  
+  }
+ 
+    
+  
+
+
 }
+
+
+
